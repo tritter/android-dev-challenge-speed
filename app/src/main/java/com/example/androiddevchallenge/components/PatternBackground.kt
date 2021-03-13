8be72cc0433b2be8fc59461e237a61e47e6530d5
Thom Ritterfeld <thom@ritterfeld.com>
2021-03-13 18:31:40 +0100

    Fixed unused imports

--- app/src/main/java/com/example/androiddevchallenge/components/PatternBackground.kt
+++ app/src/main/java/com/example/androiddevchallenge/components/PatternBackground.kt
@@ -16,14 +16,12 @@
 package com.example.androiddevchallenge.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 
